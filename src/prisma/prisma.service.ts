9ca399f47a7e4018ad418b7151dbe2cc80ac76a0
Daniel-Roch <daniel_roch4@hotmail.com>
2024-07-09 17:48:20 -0300

    chore: modific version and delete vercel

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,7 +4,12 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+    } catch (error) {
+      console.error('Error connecting to the database', error);
+      process.exit(1);
+    }
   }
   async enableShutdownHooks(app: INestApplication) {
     this.$on(`beforeExit`, async () => {
